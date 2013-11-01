7f7e8f51894575bcc529eaad74793bd7c056e3d4
JanO Mnemonix <code@7an0.com>
2013-11-01 10:59:36 +0100

    Update HalloSimpleServlet.java

--- 2013-kw44/HalloSimpleServlet.java
+++ 2013-kw44/HalloSimpleServlet.java
@@ -3,17 +3,20 @@ import javax.servlet.http.*;
 import java.io.*;
 
 public class HalloSimpleServlet
-	extend HttpServlet
+	extends HttpServlet
 {
+
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 		throws ServletException, IOException
 	{
-		res.setContentTyp("text/html");
-		printWriter out = res.getWriter();
+
+//		res.setContentTyp("text/html");
+
+		PrintWriter out = res.getWriter();
 
 		String name = "Mein erstes Servlet";
 
-		out.println("<html><head><title>"+name+"</title></head><body bgcolor=\"#000022\" color=\"#ff9900\">");
+		out.println("<html><head><title>"+name+"</title></head><body bgcolor=\"#9999DD\" color=\"#440000\">");
 		out.println("<p>Hallo "+name+"</p>");
 		out.println("</body></html>");
 
@@ -26,3 +29,30 @@ public class HalloSimpleServlet
 		doGet(req,res);
 	}
 }
+
+/*
+/Erstes Java Servlet
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.*;
+
+public class HalloSimpleServlet extends HttpServlet
+{
+   public void doGet(HttpServletRequest req, HttpServletResponse res)
+      throws ServletException, IOException
+   {
+      PrintWriter out =  res.getWriter();
+      String name="Mein erstes Servlet";
+      out.println("<html><head><title> "+name+"</title></head>");
+      out.println("<body bgcolor=\"#ccFFcc\"> ");
+      out.println(name);
+      out.println("<hr></body></html>");
+      out.close();
+   }
+   public void doPost(HttpServletRequest req, HttpServletResponse res)
+      throws ServletException, IOException
+   {
+      doGet(req,res);
+   }
+    
+}*/
